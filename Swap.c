476657aa2f7a2027f1ab056b90e26d8a90f85835
rajeshwari1610 <82741484+rajeshwari1610@users.noreply.github.com>
2021-04-18 08:28:37 +0530

    Swap.c
    It will inter change two numbers

---
+++ Swap.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+
+	printf("Enter two number");
+  int a,b;
+
+	scanf("%d%d",&a,&b);
+
+	//scanf("%d",&b);
+
+	a=a+b;
+
+	b=a-b;
+
+	a=a-b;
+
+	printf("%d%d",a,b);
+
+	//printf("%d",b);
+
+}
